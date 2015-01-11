2512b864c9ef1e2010ffb928342e212f3db63142
Onur Küçükkeçe <onurkucukkece@gmail.com>
2015-01-11 13:37:43 +0200

    Added user_id to counter model Made active relations added autosave option and before create method to user model for counter

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_one :counter, :autosave => true
+  before_create :build_counter
 end
