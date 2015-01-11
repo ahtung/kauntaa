2c1d822886a79183766049e0db9b66f7c3844c31
Dunya Kirkali <dunyakirkali@gmail.com>
2015-01-11 18:34:03 +0100

    configure test suite

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,13 +6,7 @@ require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'shoulda/matchers'
-require 'capybara/poltergeist'
 require 'helpers'
-
-Capybara.register_driver :poltergeist do |app|
-    Capybara::Poltergeist::Driver.new(app, js_errors: false)
-end
-Capybara.javascript_driver = :poltergeist
 
 ActiveRecord::Migration.maintain_test_schema!
 
@@ -20,7 +14,7 @@ RSpec.configure do |config|
 
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   config.infer_spec_type_from_file_location!
 
