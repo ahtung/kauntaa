aa96ba6c4dd2b43de40c6ec59b8d1c3fd8f15bd1
Onur Küçükkeçe <onurkucukkece@gmail.com>
2015-01-11 14:10:31 +0200

    removed autosave option from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_one :counter, :autosave => true
+  has_one :counter
   before_create :build_counter
 end
