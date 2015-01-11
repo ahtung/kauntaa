5ab9ed2bbec080f094dbda68a80f9063f591f555
Dunya Kirkali <dunyakirkali@gmail.com>
2015-01-11 12:35:13 +0100

    green for omniuth

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
-RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe User, type: :model do
+
+  describe '.find_for_google_oauth2' do
+    it 'should create a new user if does not exist' do
+      user = build(:user)
+      google_response = OmniAuth::AuthHash.new({
+        provider: 'google',
+        uid: '1337',
+        info: {
+          email: user.email,
+          password: user.password
+        }
+      })
+      User.find_for_google_oauth2(google_response, nil)
+      expect(User.count).to be(1)
+    end
+
+    it "should return user if exists" do
+      user = create(:user)
+      google_response = OmniAuth::AuthHash.new({
+        provider: 'google',
+        uid: '1337',
+        info: {
+          email: user.email,
+          password: '123$%^asd.'
+        }
+      })
+      expect(User.find_for_google_oauth2(google_response, nil)).to eq(user)
+    end
+  end
 end
