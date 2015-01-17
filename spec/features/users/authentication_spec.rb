0617dcb2c685728480a3aece3d918b90a481067b
Dunya Kirkali <dunyakirkali@gmail.com>
2015-01-17 22:18:50 +0100

    back to green

--- spec/features/users/authentication_spec.rb
+++ spec/features/users/authentication_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
-Rspec.describe 'User', type: :feature do
+RSpec.describe 'User', type: :feature do
+  let(:user) { create(:user) }
+  before :each do
+    login_as(user)
+  end
+
+  it 'should be able to logout' do
+    visit user_root_path
+    click_on 'Logout'
+    expect(page).to have_content('Count anything')
+  end
 
 end
