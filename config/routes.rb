2b802f2634c6f994d401294e0c22e100235cac72
Dunya Kirkali <dunyakirkali@gmail.com>
2015-01-25 14:37:19 +0100

    deferring user policy to another time

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'pages/about'
 
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'registrations' }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   authenticated :user do
     root 'pages#home', as: :user_root
