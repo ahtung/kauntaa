a5500f0a38f6f1cc3d92a444639e209eb1e0b529
Dunya Kirkali <dunyakirkali@gmail.com>
2015-01-25 14:33:41 +0100

    overriding devise registrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'pages/about'
 
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'registrations' }
 
   authenticated :user do
     root 'pages#home', as: :user_root
