e45a6ea1fa99822a21718b3b2728bdcf1990f570
Onur Küçükkeçe <onurkucukkece@gmail.com>
2015-02-02 21:13:57 +0200

    commented a line for future edit

--- app/models/counter.rb
+++ app/models/counter.rb
@@ -26,7 +26,8 @@ class Counter < ActiveRecord::Base
   def set_creation_date
     if self.created_at_date && self.created_at_time
       self.created_at = "#{self.created_at_date} #{self.created_at_time}:00".to_time
-      self.created_at.utc
+      # TODO When a timezone is defined, will use below
+      # self.created_at.utc
     end
   end
 end
