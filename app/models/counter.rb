dfefa0d55e34ceda4b23fc1b901bf158ad6a090f
Onur Küçükkeçe <onurkucukkece@gmail.com>
2015-02-02 21:08:26 +0200

    Added created_at date time fields

--- app/models/counter.rb
+++ app/models/counter.rb
@@ -2,6 +2,8 @@ class Counter < ActiveRecord::Base
   belongs_to :user
 
   before_save :set_name
+  before_update :set_creation_date
+  attr_accessor :created_at_date, :created_at_time
 
   def clean_value
     if value
@@ -19,5 +21,12 @@ class Counter < ActiveRecord::Base
 
   def set_name
     update_attribute(:name, 'TODO') unless name
+  end
+
+  def set_creation_date
+    if self.created_at_date && self.created_at_time
+      self.created_at = "#{self.created_at_date} #{self.created_at_time}:00".to_time
+      self.created_at.utc
+    end
   end
 end
