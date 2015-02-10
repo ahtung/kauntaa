1e0929d8b0ec08b0c09d76a62127cab4576a9db7
Dunya Kirkali <dunyakirkali@gmail.com>
2015-02-10 20:41:05 +0100

    clean up

--- app/models/counter.rb
+++ app/models/counter.rb
@@ -26,7 +26,7 @@ class Counter < ActiveRecord::Base
 
   def set_creation_date
     if self.created_at_date && self.created_at_time
-      self.created_at = "#{self.created_at_date} #{self.created_at_time}:00".to_time
+      self.created_at = DateTime.parse("#{created_at_date} #{created_at_time}")
     end
   end
 end
