3be944fb098b762c7760494e2ef36d9d0348d33c
Onur Küçükkeçe <onurkucukkece@gmail.com>
2015-03-08 17:58:09 +0200

    added new traits to user & counter factory

--- spec/factories/counters.rb
+++ spec/factories/counters.rb
@@ -2,5 +2,9 @@ FactoryGirl.define do
   factory :counter do
     name { Faker::Name.name }
     value 29
+
+    trait :a_month_older do
+      created_at { Time.now - 1.month }
+    end
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,4 +3,8 @@ FactoryGirl.define do
     email { Faker::Internet.email }
     password { Faker::Internet.password(8) }
   end
+
+  trait :with_counter_a_month_old do
+    counters { create_list(:counter, 1, :a_month_older) }
+  end
 end
