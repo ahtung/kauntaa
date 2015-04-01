2286cd16ea200fa15c5a3847395d75edfb33253e
Dunya Kirkali <dunyakirkali@gmail.com>
2015-04-01 23:33:11 +0200

    ry

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,12 @@ Palette.delete_all
   ['#E13334', '#005397', '#E13334'],
   ['#FFFFFF', '#33B67A', '#FFFFFF'],
   ['#000000', '#FACAC0', '#FFFFFF'],
+
+  ['#CEECF8', '#F4C9DD', '#FBFAEF'],
+  ['#E5625E', '#0BBCD6', '#FBFAEF'],
+  ['#FFFFFF', '#BFB5D7', '#FBFAEF'],
+  ['#2D1FE8', '#BEA1A5', '#FBFAEF'],
+  ['#EBE8E1', '#F0CE60', '#FBFAEF'],
 ].each do |palette|
   Palette.create(foreground_color: palette[0], background_color: palette[1], text_color: palette[2])
 end
