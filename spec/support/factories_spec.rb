dd49363a3032221fca4a27bd7ab9496aad722f14
Dunya Kirkali <dunyakirkali@gmail.com>
2015-04-02 22:50:46 +0200

    style improvements

--- spec/support/factories_spec.rb
+++ spec/support/factories_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe "Factory Girl" do
         factory = FactoryGirl.build(factory_name)
         if factory.respond_to?(:valid?)
           # the lamba syntax only works with rspec 2.14 or newer;  for earlier versions, you have to call #valid? before calling the matcher, otherwise the errors will be empty
-          expect(factory).to be_valid, lambda { factory.errors.full_messages.join("\n") }
+          expect(factory).to be_valid, -> { factory.errors.full_messages.join("\n") }
         end
       end
 
@@ -23,7 +23,7 @@ RSpec.describe "Factory Girl" do
           it "is valid" do
             factory = FactoryGirl.build(factory_name, trait_name)
             if factory.respond_to?(:valid?)
-              expect(factory).to be_valid, lambda { factory.errors.full_messages.join("\n") }
+              expect(factory).to be_valid, -> { factory.errors.full_messages.join("\n") }
             end
           end
         end
