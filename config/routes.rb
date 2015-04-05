f38d7229a6967efa8df25e648a5eda1bd3b24f27
Dunya Kirkali <dunyakirkali@gmail.com>
2015-04-05 23:15:36 +0200

    add route to sitemap

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   mount_roboto
   get 'pages/about'
 
+  constraints(format: 'xml') do
+    get '/sitemap', to: redirect('https://s3.eu-central-1.amazonaws.com/kauntaa/sitemaps/sitemap.xml.gz')
+  end
+
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   authenticated :user do
