3ad67a601733a64552e856c7f706af528f06f119
Onur Küçükkeçe <onurkucukkece@gmail.com>
2015-05-22 10:36:19 +0200

    added # to color in palette factory

--- spec/factories/palettes.rb
+++ spec/factories/palettes.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :palette do
-    foreground_color { format('%06x', (rand * 0xffffff)) }
-    background_color { format('%06x', (rand * 0xffffff)) }
-    text_color { format('%06x', (rand * 0xffffff)) }
+    foreground_color { "##{format('%06x', (rand * 0xffffff))}" }
+    background_color { "##{format('%06x', (rand * 0xffffff))}" }
+    text_color { "##{format('%06x', (rand * 0xffffff))}" }
   end
 end
