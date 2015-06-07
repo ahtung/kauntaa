8776834a7f4d309deb7b7407e9290a90d67105ac
Dunya Kirkali <dunyakirkali@gmail.com>
2015-06-07 17:31:56 +0200

    raise exception so that circle ci stops

--- features/support/env.rb
+++ features/support/env.rb
@@ -66,3 +66,12 @@ end
 # The :transaction strategy is faster, but might give you threading problems.
 # See https://github.com/cucumber/cucumber-rails/blob/master/features/choose_javascript_database_strategy.feature
 Cucumber::Rails::Database.javascript_strategy = :truncation
+
+at_exit do
+  begin
+    e = $! # last exception
+    cleanup_after_tests
+  ensure
+    raise e if $! != e
+  end
+end
