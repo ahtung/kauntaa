a3b9ace66b581e81570b30a67f41fde69ae16ea9
Dunya Kirkali <dunyakirkali@gmail.com>
2015-07-04 15:45:39 +0200

    boyscouting

--- features/step_definitions/increment_steps.rb
+++ features/step_definitions/increment_steps.rb
@@ -6,21 +6,22 @@ Given(/^User home page$/) do
 end
 
 When(/^I visit click to a counter$/) do
-  first('.counter').trigger('click')
+  find('.counter', match: :first).click
+  sleep 0.3
 end
 
 When(/^I click on a description of a counter$/) do
-  first('.edit-counter').trigger('click')
+  find('.edit-counter', match: :first).click
 end
 
 Then(/^counter should not have changed$/) do
-  sleep 3
-  counter_value = first('.counter').first('.counter-value').native.all_text.to_i
+  counter_value = first('.counter-value').text.to_i
   expect(counter_value).to eq(0)
 end
 
 Then(/^counter should have increased by (\d+)$/) do |increment|
-  sleep 3
-  counter_value = first('.counter').first('.counter-value').native.all_text.to_i
-  expect(counter_value).to eq(increment.to_i)
+  within "#chart" do
+    counter_value = first(".counter").first('.counter-value').text.to_i
+    expect(counter_value).to eq(increment.to_i)
+  end
 end
