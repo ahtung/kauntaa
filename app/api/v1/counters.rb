dba6bac6ce5658fe955a4573cca54ca44e5c82b4
Dunya Kirkali <dunyakirkali@gmail.com>
2015-07-11 13:06:22 +0200

    fixing api routes

--- app/api/v1/counters.rb
+++ app/api/v1/counters.rb
@@ -4,23 +4,38 @@ module V1
     desc "Lists users' counters"
     resource :users do
       segment '/:user_id' do
-        get '/counters' do
-          authenticate!
-          present User.find(params[:user_id]).counters.includes(:user).includes(:palette), with: CounterEntity if params[:user_id].to_i > 0
-        end
-      end
-    end
+        resources :counters do
 
-    resource :counters do
-      desc 'Increment counter'
-      params do
-        requires :id, type: Integer, desc: 'Counter id.'
-      end
-      route_param :id do
-        get do
-          authenticate!
-          counter = Counter.find(params[:id])
-          counter.increment
+          desc 'List counters'
+          get do
+            authenticate!
+            present current_user.counters.includes(:user).includes(:palette), with: CounterEntity
+          end
+
+          desc 'Increment counter'
+          params do
+            requires :id, type: Integer, desc: 'Counter id.'
+          end
+          route_param :id do
+            get :increment do
+              authenticate!
+              counter = current_user.counters.includes(:user).includes(:palette).find(params[:id])
+              counter.increment
+              present counter, with: CounterEntity
+            end
+          end
+
+          desc 'Read counter'
+          params do
+            requires :id, type: Integer, desc: 'Counter id.'
+          end
+          route_param :id do
+            get do
+              authenticate!
+              counter = current_user.counters.includes(:user).includes(:palette).find(params[:id])
+              present counter, with: CounterEntity
+            end
+          end
         end
       end
     end
--- spec/requests/api_v1_spec.rb
+++ spec/requests/api_v1_spec.rb
@@ -1,41 +1,56 @@
 require 'rails_helper'
 
-RSpec.describe API, type: :request do
+RSpec.describe API, type: :request, focus: true do
+  let!(:user) { create(:user, :with_counters) }
+
   context 'unauthenticated user' do
-    describe 'GET /api/v1/counters/:id' do
+    describe 'GET /api/v1/users/:user_id/counters/:id' do
       it 'redirects' do
         counter = create(:counter)
-        get "/api/v1/counters/#{counter.id}", format: :json
+        get "/api/v1/users/#{user.id}/counters/#{user.counters.first.id}", format: :json
         expect(response.body).to include 'You need to sign in or sign up before continuing'
       end
     end
 
     describe 'GET /api/v1/users/:user_id/counters' do
       it 'redirects' do
-        user = create(:user, :with_counters)
         get "/api/v1/users/#{user.id}/counters", format: :json
+        expect(response.body).to include 'You need to sign in or sign up before continuing'
+      end
+    end
+
+    describe 'GET /api/v1/users/:user_id/counters/:id/increment' do
+      it "redirects" do
+        get "/api/v1/users/#{user.id}/counters/#{user.counters.first.id}/increment", format: :json
         expect(response.body).to include 'You need to sign in or sign up before continuing'
       end
     end
   end
 
   context 'authenticated user' do
-    let(:user) { create(:user, :with_counters) }
+
 
     before :each do
       post user_session_path, user: { email: user.email, password: user.password }
     end
 
-    describe 'GET /api/v1/counters/:id' do
-      it 'returns true' do
-        get "/api/v1/counters/#{user.counters.first.id}", format: :json
-        expect(response.body).to eq 'true'
+    describe 'GET /api/v1/users/:user_id/counters/:id' do
+      it "returns user's counter" do
+        get "/api/v1/users/#{user.id}/counters/#{user.counters.first.id}", format: :json
+        expect(response.status).to eq 200
       end
     end
 
     describe 'GET /api/v1/users/:user_id/counters' do
-      it "lists user's counter" do
+      it "Lists user's counters" do
         get "/api/v1/users/#{user.id}/counters", format: :json
+        expect(response.status).to eq 200
+      end
+    end
+
+    describe 'GET /api/v1/users/:user_id/counters/:id/increment' do
+      it "Increment user's counter" do
+        get "/api/v1/users/#{user.id}/counters/#{user.counters.first.id}/increment", format: :json
         expect(response.status).to eq 200
       end
     end
