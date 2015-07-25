896247b24890026d51d4f32e7b78b99e46875c2a
Dunya Kirkali <dunyakirkali@gmail.com>
2015-07-25 12:42:01 +0200

    Update application_helper_spec.rb

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 describe ApplicationHelper do
   describe "#colored_link_to" do
     it "returns colored link with parameters" do
-      palette = FactoryGirl.create(:palette)
+      palette = create(:palette)
       parameters = [
-        id: 'home-link',
-        class: 'home-link',
+        id: "home-link",
+        class: "home-link",
         remote: true
       ]
       parameters.each do |key, value|
-        helper.colored_link_to('Home', '/', {key: value, palette: palette}).should eq(link_to('Home', '/', style: "color: #{palette.text_color}", key: value))
+        expect(helper.colored_link_to("Home", "/", {key: value, palette: palette})).to eq(link_to('Home', '/', style: "color: #{palette.text_color}", key: value))
       end
     end
   end
