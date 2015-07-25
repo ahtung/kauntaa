87be4860e00cf9754570fd3f0435fff0b1932faf
Onur Küçükkeçe <onurkucukkece@gmail.com>
2015-07-25 13:18:27 +0300

    colored_link_to params assimilated to link_to method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 # ApplicationHelper
 module ApplicationHelper
-  def colored_link_to(options = {})
-    link_to(options[:link_text], options[:link], class: options[:class], style: "color: #{options[:palette].text_color}", id: options[:id], remote: options[:remote])
+  def colored_link_to(link_text, link, options = {})
+    link_to link_text, link, class: options[:class], style: "color: #{options[:palette].text_color}", id: options[:id], remote: options[:remote]
   end
 end
