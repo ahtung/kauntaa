ca49ff9d23729afe8e323c1f083777d943cd0694
Dunya Kirkali <dunyakirkali@gmail.com>
2015-07-25 13:25:50 +0200

    boyscouting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,10 @@
 # ApplicationHelper
 module ApplicationHelper
   def colored_link_to(link_text, link, options = {})
-    link_to link_text, link, class: options[:class], style: "color: #{options[:palette][:text_color]}; background-color: #{options[:palette][:foreground_color]};", id: options[:id], remote: options[:remote]
+    link_to link_text, link,
+            class: options[:class],
+            style: "color: #{options[:palette][:text_color]}; background-color: #{options[:palette][:foreground_color]};",
+            id: options[:id],
+            remote: options[:remote]
   end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -5,15 +5,21 @@ RSpec.describe ApplicationHelper, type: :helper do
     let(:palette) { create(:palette) }
     it 'returns colored link with parameters id eq home-link' do
       expect(
-        helper.colored_link_to('Home', '/',{ id: 'home-link', palette: { text_color: palette.text_color, foreground_color: palette.foreground_color } })
+        helper.colored_link_to(
+          'Home',
+          '/',
+          id: 'home-link', palette: { text_color: palette.text_color, foreground_color: palette.foreground_color })
       ).to eq(
-        link_to('Home', '/', style: "color: #{palette.text_color}; background-color: #{palette.foreground_color};", id: "home-link")
+        link_to('Home', '/', style: "color: #{palette.text_color}; background-color: #{palette.foreground_color};", id: 'home-link')
       )
     end
 
     it 'returns colored link with parameters class eq home-link' do
       expect(
-        helper.colored_link_to('Home', '/', { class: 'home-link', palette: { text_color: palette.text_color, foreground_color: palette.foreground_color } })
+        helper.colored_link_to(
+          'Home',
+          '/',
+          class: 'home-link', palette: { text_color: palette.text_color, foreground_color: palette.foreground_color })
       ).to eq(
         link_to('Home', '/', class: 'home-link', style: "color: #{palette.text_color}; background-color: #{palette.foreground_color};")
       )
@@ -21,7 +27,10 @@ RSpec.describe ApplicationHelper, type: :helper do
 
     it 'returns colored link with parameters remote eq true' do
       expect(
-        helper.colored_link_to('Home', '/', remote: true, palette: { text_color: palette.text_color, foreground_color: palette.foreground_color })
+        helper.colored_link_to(
+          'Home',
+          '/',
+          remote: true, palette: { text_color: palette.text_color, foreground_color: palette.foreground_color })
       ).to eq(
         link_to('Home', '/', style: "color: #{palette.text_color}; background-color: #{palette.foreground_color};", remote: true)
       )
