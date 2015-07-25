4022c8a57f437ef3f530e9cebf9c1e555c16761f
Onur Küçükkeçe <onurkucukkece@gmail.com>
2015-07-25 13:04:38 +0300

    added colored_link helper to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
 # ApplicationHelper
 module ApplicationHelper
+  def colored_link(options = {})
+    link_to(options[:link_text], options[:link], class: options[:class], style: "color: #{options[:palette].text_color}", id: options[:id], remote: options[:remote])
+  end
 end
