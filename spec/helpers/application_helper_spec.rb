2cf90f1c24663c3e76f94bf4bf742cb597188682
Dunya Kirkali <dunyakirkali@gmail.com>
2015-07-25 13:21:56 +0200

    boyscouting

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe ApplicationHelper, type: :helper do
   describe '#colored_link_to' do
     let(:palette) { create(:palette) }
-    it "returns colored link with parameters id eq home-link" do
+    it 'returns colored link with parameters id eq home-link' do
       expect(
         helper.colored_link_to('Home', '/',{ id: 'home-link', palette: { text_color: palette.text_color, foreground_color: palette.foreground_color } })
       ).to eq(
@@ -11,17 +11,17 @@ RSpec.describe ApplicationHelper, type: :helper do
       )
     end
 
-    it "returns colored link with parameters class eq home-link" do
+    it 'returns colored link with parameters class eq home-link' do
       expect(
-        helper.colored_link_to('Home', '/',{ class: 'home-link', palette: { text_color: palette.text_color, foreground_color: palette.foreground_color } })
+        helper.colored_link_to('Home', '/', { class: 'home-link', palette: { text_color: palette.text_color, foreground_color: palette.foreground_color } })
       ).to eq(
-        link_to('Home', '/', class: "home-link", style: "color: #{palette.text_color}; background-color: #{palette.foreground_color};")
+        link_to('Home', '/', class: 'home-link', style: "color: #{palette.text_color}; background-color: #{palette.foreground_color};")
       )
     end
 
-    it "returns colored link with parameters remote eq true" do
+    it 'returns colored link with parameters remote eq true' do
       expect(
-        helper.colored_link_to('Home', '/',{ remote: true, palette: { text_color: palette.text_color, foreground_color: palette.foreground_color } })
+        helper.colored_link_to('Home', '/', remote: true, palette: { text_color: palette.text_color, foreground_color: palette.foreground_color })
       ).to eq(
         link_to('Home', '/', style: "color: #{palette.text_color}; background-color: #{palette.foreground_color};", remote: true)
       )
