b6924e7251313e3acb66b10cc1885e853b14392a
Onur Küçükkeçe <onurkucukkece@gmail.com>
2015-07-25 14:02:54 +0300

    colored_link_to changed according to new syntaxes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 # ApplicationHelper
 module ApplicationHelper
   def colored_link_to(link_text, link, options = {})
-    link_to link_text, link, class: options[:class], style: "color: #{options[:palette].text_color}", id: options[:id], remote: options[:remote]
+    link_to link_text, link, class: options[:class], style: "color: #{options[:palette][:text_color]}; background: #{options[:palette][:foreground_color]};", id: options[:id], remote: options[:remote]
   end
 end
