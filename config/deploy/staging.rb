d15fe9e516d21cc8c12b55e081988cdd46ea0f35
Dunya Kirkali <dunyakirkali@gmail.com>
2015-08-01 19:18:25 +0200

    correct puma env

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,7 +2,7 @@ server '104.155.32.122', user: 'dunyakirkali', roles: %w(app db web)
 
 set :stage, :staging
 set :rails_env, 'staging'
-set :puma_env, 'staging'
+set :puma_env, 'production'
 set :nginx_domains, 'staging.kauntaa.com'
 set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma-#{fetch :application}.sock"
 set :app_server_socket, "#{shared_path}/tmp/sockets/puma-#{fetch :application}.sock"
