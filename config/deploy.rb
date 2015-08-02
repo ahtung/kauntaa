91da1f541a3cb0d28d0de30671b7b90df704da95
Dunya Kirkali <dunyakirkali@gmail.com>
2015-08-02 14:15:54 +0200

    set foreman env path

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,6 +12,7 @@ set :deploy_to, '/var/www/kauntaa'
 set :rvm_type, :system
 set :rvm_ruby_version, '2.1.2'
 set :whenever_identifier, -> { "#{fetch(:application)}_#{fetch(:stage)}" }
+set :foreman_env,  '/var/www/kauntaa/shared/config/.env'
 
 # Default value for :pty is false
 # set :pty, true
