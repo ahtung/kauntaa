a1592b480b1d7852705b42557689bb5e1f750454
Dunya Kirkali <dunyakirkali@gmail.com>
2015-08-16 14:45:52 +0200

    Remove cap docs

--- config/deploy/production.rb
+++
@@ -1,8 +0,0 @@
-server '104.155.2.29', user: 'deployer', roles: %w(app db web)
-
-set :stage, :production
-set :rails_env, 'production'
-set :puma_env, 'production'
-set :nginx_domains, 'www.kauntaa.com kauntaa.com'
-set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
-set :app_server_socket, "#{shared_path}/tmp/sockets/puma.sock"
